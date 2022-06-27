39be3e0a7a2f889562686003745395956f386775
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-06-27 14:49:54 -0700

    defining 2 type for auto and manual placement

---
+++ players/load/load_player.h
@@ -0,0 +1,11 @@
+#ifndef SEA_BATTLE_LOAD_PLAYER_H
+#define SEA_BATTLE_LOAD_PLAYER_H
+
+#include "../../auto_import.h"
+
+Player loadPlayerFromUserChoice(int selectUserOrder, int putShipOrder);
+
+
+Player handleAutoShipPlacement(User user);
+
+#endif //SEA_BATTLE_LOAD_USER_H
